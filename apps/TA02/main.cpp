1ac259fb6c7e28c09922e2093b6da9bc4e0e9f8d
zhaoyoubing <zhaoyoubing@gmail.com>
2026-08-26 23:44:18 +0800

    rename Pipeline to CPUPipeline for TA02

--- apps/TA02/main.cpp
+++ apps/TA02/main.cpp
@@ -9,7 +9,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/transform.hpp>
 
-#include "render/Pipeline.h"
+#include "render/GPUPipeline.h"
 // #include "drawColourVertex.h"
 
 #include "ecs/EcsTypes.h"
@@ -65,7 +65,7 @@ int main() {
     
     // Group the compiled stages into a Pipeline (Vertex-Fragment pair)
     std::vector<Shader*> shaderStages = { &vertShader, &fragShader };
-    Pipeline basicPipeline(shaderStages);
+    GPUPipeline basicPipeline(shaderStages);
     
 
     // ================ Model Setup ================
