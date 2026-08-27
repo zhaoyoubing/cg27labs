25602184537758255184f7e2a6307482136750f3
zhaoyoubing <zhaoyoubing@gmail.com>
2026-08-27 22:15:01 +0800

    Fixed error in EntityFactory.h (not in use)

--- include/ecs/entity/EntityFactory.h
+++ include/ecs/entity/EntityFactory.h
@@ -1,6 +1,7 @@
 // include/ecs/entity/EntityFactory.h
 #pragma once
 #include "ecs/EcsTypes.h"
+#include "ecs/EcsWorldRegistry.h"
 #include "ecs/components/TransformComp.h"
 #include "ecs/components/CameraComp.h"
 
@@ -9,26 +10,26 @@
 
 class EntityFactory {
 public:
-    static EntityID createCamera(ECSWorldPool& world, 
+    static EntityID createCamera(ECSWorldRegistry & world, 
         glm::vec3 pos = glm::vec3(0.0f, 0.0f, 3.0f)) {
         // 1. Create a raw entity handle from your ECS world pool
         EntityID cam = world.CreateEntity();
 
         // 2. Add the TransformComponent to define WHERE the camera is in the world
         world.addComp<TransformComponent>(cam, TransformComp{
-            .position = pos,
-            .rotation = glm::vec3(0.0f),
+            .pos = pos,
+            .rot = glm::vec3(0.0f),
             .scale = glm::vec3(1.0f)
         });
 
         // 3. Add the CameraComponent to define HOW it views the world (FOV, planes, angles)
         world.addComp<CameraComponent>(cam, CameraComp{
             .fov = 45.0f,
-            .nearPlane = 0.1f,
-            .farPlane = 100.0f,
+            .near = 0.1f,
+            .far = 100.0f,
             .yaw = -90.0f, // Looking down the negative Z-axis by default
             .pitch = 0.0f,
-            //.sensitivity = 0.1f
+            .sensitivity = 0.1f
         });
 
         return cameraEntity;
