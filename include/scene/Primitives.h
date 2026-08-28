d72dffd5f86c33156f293c2a40b064dac15e365e
zhaoyoubing <zhaoyoubing@gmail.com>
2026-08-28 14:29:25 +0800

    update Vertex comments

--- include/scene/Primitives.h
+++ include/scene/Primitives.h
@@ -8,7 +8,7 @@
 struct Vertex {
     glm::vec3 pos;       // 12 bytes
     glm::vec3 normal;    // 12 bytes
-    glm::vec3 tagent;    // 12 bytes
-    glm::vec2 uv;        // 8 bytes
-    glm::vec2 uv1;       // 8 bytes
+    glm::vec3 tagent;    // 12 bytes, for normal mapping
+    glm::vec2 uv;        // 8 bytes, texture coordinates
+    glm::vec2 uv1;       // 8 bytes, not in use
 };
